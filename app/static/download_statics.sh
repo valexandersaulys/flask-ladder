9c6caf46e58a687dc0a9e8d9cb5891fc7002beef
Vincent A. Saulys <valexandersaulys@gmail.com>
2016-10-17 10:41:26 -0400

    created a wget script to download javascript libraries

---
+++ app/static/download_statics.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Bash script to download the different javascript and css files
+# Updated as of 10 - 17 - 2016
+
+# Minified & Zipped Downloads
+wget https://code.jquery.com/jquery-3.1.1.min.js
+wget http://backbonejs.org/backbone-min.js
+wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css
+wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css #optional
+wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js #optional
+
+# Also, the non-min versions for development (if useful)
+#wget https://code.jquery.com/jquery-3.1.1.js
+#wget http://backbonejs.org/backbone.js
+#wget https://github.com/twbs/bootstrap/releases/download/v3.3.7/bootstrap-3.3.7-dist.zip
