4d3f42e21d4b6268a5aff803adbe2fb3b05e31bc
Vincent A. Saulys <valexandersaulys@gmail.com>
2016-10-17 11:52:53 -0400

    added underscore to download script

--- app/static/download_statics.sh
+++ app/static/download_statics.sh
@@ -9,8 +9,10 @@ wget http://backbonejs.org/backbone-min.js
 wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css
 wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css #optional
 wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js #optional
+wget http://underscorejs.org/underscore-min.js
 
 # Also, the non-min versions for development (if useful)
 #wget https://code.jquery.com/jquery-3.1.1.js
 #wget http://backbonejs.org/backbone.js
 #wget https://github.com/twbs/bootstrap/releases/download/v3.3.7/bootstrap-3.3.7-dist.zip
+#wget http://underscorejs.org/underscore.js
